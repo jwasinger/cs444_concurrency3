c809d7062c3ccccc0e3f8b41921b71fef04dbed9
Jared Wasinger <j-wasinger@hotmail.com>
2017-05-16 14:22:17 -0700

    initial working concurrency code to provide 3-way mutual exclusion

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
   "fmt"
+  "sync"
+  "time"
 )
 
 /*
@@ -12,16 +14,54 @@ Inserters add new items to the end of the list; insertions must be mutually excl
 However, one insert can proceed in parallel with any number of searches. Finally, deleters remove items from anywhere in the list. At most one deleter process can access the list at a time, and deletion must also be mutually exclusive with searches and insertions.
 */
 
-func searcher() {
+func searcher(search_wg *sync.WaitGroup, delete_wg *sync.WaitGroup) {
+  delete_wg.Wait()
+  search_wg.Add(1)
+  
+  fmt.Println("searching....")
+  time.Sleep(5*time.Second)
 
+  search_wg.Done()
+
+  fmt.Println("search done...")
+  time.Sleep(5*time.Second)
 }
 
-func deleter() {
+func inserter(im *sync.Mutex, insert_wg *sync.WaitGroup, delete_wg *sync.WaitGroup) {
+  delete_wg.Wait()
+  insert_wg.Add(1)
 
+  im.Lock()
+
+  fmt.Println("inserting.... ")
+
+  time.Sleep(5*time.Second)
+
+  im.Unlock()
+  insert_wg.Done()
+
+  fmt.Println("inserting done...")
+  time.Sleep(5*time.Second)
 }
 
-func inserter() {
+func deleter(dm *sync.Mutex, search_wg *sync.WaitGroup, delete_wg *sync.WaitGroup, insert_wg *sync.WaitGroup) {
+  search_wg.Wait()
+  insert_wg.Wait()
 
+  //delete_wg.Wait()
+
+  delete_wg.Add(1)
+  dm.Lock()
+
+  fmt.Println("Deleting....")
+
+  time.Sleep(5*time.Second)
+
+  fmt.Println("asdfasdfasd")
+
+  dm.Unlock()
+  
+  delete_wg.Done()
 }
 
 func main() {
@@ -33,4 +73,25 @@ func main() {
   list.Delete(1)
   list.Print()
   fmt.Println("done")
+  
+  var n int = 5 // 5 of each type of thread
+  
+  search_wg := &sync.WaitGroup{}
+  insert_wg := &sync.WaitGroup{}
+  delete_wg := &sync.WaitGroup{}
+  im :=        &sync.Mutex{}
+  dm :=        &sync.Mutex{}
+
+    go searcher(search_wg, delete_wg)
+    go searcher(search_wg, delete_wg)
+    go inserter(im, insert_wg, delete_wg)
+    go inserter(im, insert_wg, delete_wg)
+    go deleter(dm, search_wg, delete_wg, insert_wg)
+    go deleter(dm, search_wg, delete_wg, insert_wg)
+
+  fmt.Println(n)
+
+  wg := &sync.WaitGroup{}
+  wg.Add(1)
+  wg.Wait()
 }

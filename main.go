683ef1a146cb88a1a70f1efbaa1a0cf1f0f56fc9
Jared Wasinger <j-wasinger@hotmail.com>
2017-05-16 15:34:43 -0700

    first working edition

--- main.go
+++ main.go
@@ -2,96 +2,85 @@ package main
 
 import (
   "fmt"
-  "sync"
   "time"
+  "sync"
 )
 
-/*
-Three kinds of threads share access to a singly-linked list: searchers, inserters and deleters. Searchers merely examine the list; hence they can execute concurrently with each other.
-
-Inserters add new items to the end of the list; insertions must be mutually exclusive to preclude two inserters from inserting new items at about the same time.
-
-However, one insert can proceed in parallel with any number of searches. Finally, deleters remove items from anywhere in the list. At most one deleter process can access the list at a time, and deletion must also be mutually exclusive with searches and insertions.
-*/
-
-func searcher(search_wg *sync.WaitGroup, delete_wg *sync.WaitGroup) {
-  delete_wg.Wait()
-  search_wg.Add(1)
-  
-  fmt.Println("searching....")
-  time.Sleep(5*time.Second)
-
-  search_wg.Done()
-
-  fmt.Println("search done...")
-  time.Sleep(5*time.Second)
-}
-
-func inserter(im *sync.Mutex, insert_wg *sync.WaitGroup, delete_wg *sync.WaitGroup) {
-  delete_wg.Wait()
-  insert_wg.Add(1)
-
-  im.Lock()
-
-  fmt.Println("inserting.... ")
-
-  time.Sleep(5*time.Second)
-
-  im.Unlock()
-  insert_wg.Done()
-
-  fmt.Println("inserting done...")
-  time.Sleep(5*time.Second)
-}
-
-func deleter(dm *sync.Mutex, search_wg *sync.WaitGroup, delete_wg *sync.WaitGroup, insert_wg *sync.WaitGroup) {
-  search_wg.Wait()
-  insert_wg.Wait()
-
-  //delete_wg.Wait()
-
-  delete_wg.Add(1)
-  dm.Lock()
-
-  fmt.Println("Deleting....")
-
-  time.Sleep(5*time.Second)
-
-  fmt.Println("asdfasdfasd")
-
-  dm.Unlock()
-  
-  delete_wg.Done()
-}
-
 func main() {
-  list := new(LinkedList)
-  list.PushEnd(0)
-  list.PushEnd(1)
-  list.PushEnd(2)
-  list.Delete(0)
-  list.Delete(1)
-  list.Print()
-  fmt.Println("done")
-  
-  var n int = 5 // 5 of each type of thread
-  
-  search_wg := &sync.WaitGroup{}
+  l := &LinkedList{}
+
   insert_wg := &sync.WaitGroup{}
   delete_wg := &sync.WaitGroup{}
-  im :=        &sync.Mutex{}
-  dm :=        &sync.Mutex{}
+  search_wg := &sync.WaitGroup{}
 
-    go searcher(search_wg, delete_wg)
-    go searcher(search_wg, delete_wg)
-    go inserter(im, insert_wg, delete_wg)
-    go inserter(im, insert_wg, delete_wg)
-    go deleter(dm, search_wg, delete_wg, insert_wg)
-    go deleter(dm, search_wg, delete_wg, insert_wg)
+  im := &sync.Mutex{}
+  dm := &sync.Mutex{}
 
-  fmt.Println(n)
+  numSearchers := 2
+  numDeleters := 2
+  numInserters := 2
 
-  wg := &sync.WaitGroup{}
-  wg.Add(1)
-  wg.Wait()
+  //inserters
+  for i := 0; i < numInserters; i++ {
+    go func() {
+      for ; ; {
+        //dont run while deleters are running
+        delete_wg.Wait()
+
+        insert_wg.Add(1)
+
+        fmt.Println("inserting...")
+        time.Sleep(1*time.Second)
+
+        im.Lock()
+        l.PushEnd(1)
+        fmt.Println(l.Size())
+        im.Unlock()
+
+        insert_wg.Done()
+        time.Sleep(1*time.Second)
+      }
+    } ()
+  }
+
+  //deleters
+  for i := 0; i < numDeleters; i++ {
+    go func() {
+      for ; ; {
+        insert_wg.Wait()
+        search_wg.Wait()
+        
+        delete_wg.Add(1)
+
+        fmt.Println("deleting....")
+        time.Sleep(time.Second*1)
+
+        dm.Lock()
+        l.Delete(l.Size()-1)
+        fmt.Println(l.Size())
+        dm.Unlock()
+
+        delete_wg.Done()
+        time.Sleep(1*time.Second)
+      }
+    } ()
+  }
+
+  for i := 0; i < numSearchers; i++ {
+    go func() {
+      for ; ; {
+        delete_wg.Wait()
+        search_wg.Add(1)
+
+        fmt.Println("searching... ")
+        time.Sleep(time.Second*1)
+
+        search_wg.Done()
+        time.Sleep(1*time.Second)
+      }
+    } ()
+  }
+
+  time.Sleep(5*time.Minute)
 }
+
